2e3e8905e50609e683e5182f1374884454bae8d6
Daniil Zatsepin <red-county@mail.ru>
2024-01-31 20:52:33 +0300

    Fix add_taining

--- src/python-scripts/add_training.py
+++ src/python-scripts/add_training.py
@@ -5,6 +5,7 @@ import pandas as pd
 from darts import TimeSeries
 from darts.dataprocessing.transformers import Scaler
 import os
+from confluent_kafka import Producer
 
 # Файл для логгирования 
 sys.stderr = open('log.txt', 'w')
@@ -27,11 +28,15 @@ file_id = google_drive_url.split('/d/')[1].split('/view')[0]
 
 download_url = f"https://drive.google.com/uc?id={file_id}"
 
-# Путь для сохранения файла данных
-if not os.path.exists('uploads/%s/%s' %(ulid, project)):
-    os.makedirs('uploads/%s/%s' %(ulid, project))
+print(download_url)
 
-local_file_path = "uploads/%s/%s/data.csv" %(ulid, project)
+# Путь для сохранения файла данных
+if not os.path.exists('uploads\%s\%s' %(ulid, project)):
+    os.makedirs('uploads\%s\%s' %(ulid, project))
+
+local_file_path = "uploads\%s\%s\data.csv" %(ulid, project)
+
+print(local_file_path)
 
 # Скачивание файла
 gdown.download(download_url, local_file_path, quiet=False)
@@ -45,5 +50,27 @@ scaled_train = train_scaler.fit_transform(training_data)
 
 model.fit(scaled_train, epochs = steps)
 
-# Сохранение дообученной модели
 model.save("uploads/%s/%s/model.pkl" %(ulid, project))
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Сообщение не удалось доставить: {err}")
+    else:
+        print(f"Сообщение доставлено в {msg.topic()} [{msg.partition()}]")
+
+conf = {
+    'bootstrap.servers': 'cs.rsu.edu.ru:9092',  # Список брокеров
+    'client.id': 'rrl-app'  # ID клиента
+}
+
+producer = Producer(**conf)
+
+topic = 'testTopic'
+
+try:
+    message = 'Скрипт завершил работу!'
+    producer.produce(topic, message.encode('utf-8'), callback=delivery_report)
+except Exception as e:
+    print(f"Возникла ошибка при отправке сообщения: {e}")
+
+producer.flush()
